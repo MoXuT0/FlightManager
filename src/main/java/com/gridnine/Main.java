7862537bdd5822235c30f3755a048dcf0366462b
MoXuT0 <DenisFisenko2010@yandex.ru>
2023-06-06 11:50:34 +0300

    Init commit fix

--- src/main/java/com/gridnine/Main.java
+++ src/main/java/com/gridnine/Main.java
@@ -1,4 +1,6 @@
-package com.gridnine;public class Main {
+package com.gridnine;
+
+public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
     }
